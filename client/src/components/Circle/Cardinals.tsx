9bea16fba5973c7f5f000796ebb911db08834c32
Elias <elias.db3@gmail.com>
2023-08-20 04:56:14 +0200

    added streepkes for cardinals

--- client/src/components/Circle/Cardinals.tsx
+++ client/src/components/Circle/Cardinals.tsx
@@ -2,9 +2,13 @@ const Cardinals = () => {
   return (
     <div className="cardinals">
       <span className="cardinal-N">N</span>
+      <div className="vertical-line-n"></div>
       <span className="cardinal-E">E</span>
+      <hr className="horizontal-line-e" />
       <span className="cardinal-S">S</span>
+      <div className="vertical-line-s"></div>
       <span className="cardinal-W">W</span>
+      <hr className="horizontal-line-w" />
     </div>
   );
 };
